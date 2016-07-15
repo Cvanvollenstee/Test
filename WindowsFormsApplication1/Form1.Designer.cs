f076c3a91161fa75b48aa4615da4adcaa396329e
cvanvollenstee <cvanvollenstee@cfaktor.com>
2016-07-15 11:21:28 +0200

    Some New Changes

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.lstvMessage = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(330, 139);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(153, 122);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(100, 277);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 13);
+            this.lblMessage.TabIndex = 2;
+            // 
+            // lstvMessage
+            // 
+            this.lstvMessage.FullRowSelect = true;
+            this.lstvMessage.GridLines = true;
+            this.lstvMessage.Location = new System.Drawing.Point(46, 235);
+            this.lstvMessage.Name = "lstvMessage";
+            this.lstvMessage.Size = new System.Drawing.Size(508, 242);
+            this.lstvMessage.TabIndex = 3;
+            this.lstvMessage.UseCompatibleStateImageBehavior = false;
+            this.lstvMessage.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 510);
+            this.Controls.Add(this.lstvMessage);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.ListView lstvMessage;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private const int listenPort = 80;
+
+        private UdpClient listener = new UdpClient(listenPort);
+
+        private bool done = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            
+
+
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Parse("192.168.240.170"), listenPort);
+            try
+            {
+                while (!done)
+                {
+                    lstvMessage.Items.Add("Waiting for broadcast");
+                    byte[] bytes = listener.Receive(ref groupEP);
+                    lstvMessage.Items.Add("Received broadcast from " + groupEP.ToString() + " : " + Encoding.ASCII.GetString(bytes, 0, bytes.Length) + "");
+
+                    Application.DoEvents();
+
+                    Thread.Sleep(100);
+                }
+            }
+            catch (Exception epx)
+            {
+                lblMessage.Text += epx.ToString();
+            }
+            finally
+            {
+                listener.Close();
+            }
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            done = true;
+
+            listener.Close();
+        }
+    }
+}
